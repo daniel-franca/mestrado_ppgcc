1036b29883f8bab3e95983eaf2f194c190f456cd
Daniel <31888602+daniel-franca@users.noreply.github.com>
2024-12-19 14:07:15 -0300

    Update api_ubuntu_5.py

--- api_ubuntu_5.py
+++ api_ubuntu_5.py
@@ -67,7 +67,7 @@ def get_site_data(url, ip):
 # Loop para obter os dados para cada offset
 for i in range(25000):
     for ip in ips:
-        url = f"https://ubuntu.com/security/cves.json?limit=100&offset={i}"
+        url = f"https://ubuntu.com/security/cves.json?limit=20&offset={i}"
         print(url)
         try:
             data = get_json_data(url, ip)
@@ -88,59 +88,57 @@ for i in range(25000):
                             support = str(information['packages'][a]['statuses'][b]['description'])
                             status = str(information['packages'][a]['statuses'][b]['status'])
                             # Verify if NIST has the CVE and catch date and severity
-                            #select = f"SELECT CVE, Published, Severity from nist WHERE CVE LIKE '{idcve}'"
-                            #cursor.execute(select)
-                            #records = cursor.fetchall()
-                            #for x in records:
-                            #    nist_date = x[1]
-                            #    nist_severity = x[2]
-                            # Get correction date from releases packages
-                            if status == 'released' and status != 'upstream':
-                                try:
-                                    # Mount and open URL
-                                    ubuntu_url = f"https://launchpad.net/ubuntu/+source/{package}/{support}"
-                                    ubuntu_package_page = get_site_data(ubuntu_url, ip)
-                                    # Extract HTML information
-                                    html_bytes = ubuntu_package_page
-                                    html = str(html_bytes.decode("utf-8"))
-                                    # find changes URL
-                                    change_url = str(re.findall(".*_source.changes", html))
-                                    # Extract URL
-                                    change_url = str((change_url.replace("['     <a href=", "")))
-                                    change_url = str((change_url.replace('"', "")))
-                                    change_url = str((change_url.replace("']", "")))  # Open Source Change URL
-                                    source_change_url = change_url
-                                    package_change_page = urlopen(source_change_url)
-                                    # Extract HTML information
-                                    html_bytes = package_change_page.read()
-                                    html = str(html_bytes.decode("utf-8"))
-                                    # find changes URL
-                                    package_change_url = str(re.findall("Date: .*", html))
-                                    # Extract Date
-                                    # Replace characters
-                                    resolved_date = str(package_change_url.replace("['Date: ", ""))
-                                    resolved_date = str(resolved_date.replace("']", ""))
-                                    resolved_date = str(resolved_date[5:16])
-                                    date_format = '%d %b %Y'
-                                    date_obj = str(datetime.strptime(resolved_date, date_format))
-                                    resolved_date = datetime.strptime(date_obj, "%Y-%m-%d %H:%M:%S")
-                                    resolved_date = str(resolved_date.strftime('%Y-%m-%d'))
-                                except:
-                                    resolved_date = str("CHECK MANUALLY")
-                                # Write to DataBase
-                                sql = "INSERT INTO ubuntu (CVE, Published, Priority, Package, Distro, Support, Status, Resolved) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                                values = (
-                                idcve, published, priority, package, distro, support, status, resolved_date)
-                                cursor.execute(sql, values)
-                                mydb.commit()
-                            else:
-                                # Write to DataBase
-                                sql = "INSERT INTO ubuntu (CVE, Published, Priority, Package, Distro, Support, Status) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                                values = (
-                                idcve, published, priority, package, distro, support, status)
-                                cursor.execute(sql, values)
-                                mydb.commit()
-
+                            select = f"SELECT CVE, Published, Severity from nist WHERE CVE LIKE '{idcve}'"
+                            cursor.execute(select)
+                            records = cursor.fetchall()
+                            for x in records:
+                                nist_date = x[1]
+                                nist_severity = x[2]
+                                # Get correction date from releases packages
+                                if status == 'released' and status != 'upstream':
+                                    try:
+                                        # Mount and open URL
+                                        ubuntu_url = f"https://launchpad.net/ubuntu/+source/{package}/{support}"
+                                        ubuntu_package_page = get_site_data(ubuntu_url, ip)
+                                        # Extract HTML information
+                                        html_bytes = ubuntu_package_page
+                                        html = str(html_bytes.decode("utf-8"))
+                                        # find changes URL
+                                        change_url = str(re.findall(".*_source.changes", html))
+                                        # Extract URL
+                                        change_url = str((change_url.replace("['     <a href=", "")))
+                                        change_url = str((change_url.replace('"', "")))
+                                        change_url = str((change_url.replace("']", "")))  # Open Source Change URL
+                                        source_change_url = change_url
+                                        package_change_page = urlopen(source_change_url)
+                                        # Extract HTML information
+                                        html_bytes = package_change_page.read()
+                                        html = str(html_bytes.decode("utf-8"))
+                                        # find changes URL
+                                        package_change_url = str(re.findall("Date: .*", html))
+                                        # Extract Date
+                                        # Replace characters
+                                        resolved_date = str(package_change_url.replace("['Date: ", ""))
+                                        resolved_date = str(resolved_date.replace("']", ""))
+                                        resolved_date = str(resolved_date[5:16])
+                                        date_format = '%d %b %Y'
+                                        date_obj = str(datetime.strptime(resolved_date, date_format))
+                                        resolved_date = datetime.strptime(date_obj, "%Y-%m-%d %H:%M:%S")
+                                        resolved_date = str(resolved_date.strftime('%Y-%m-%d'))
+                                    except:
+                                        resolved_date = str("CHECK MANUALLY")
+                                    # Write to DataBase
+                                    sql = "INSERT INTO ubuntu (CVE, Published, Priority, Package, Distro, Support, Status, Resolved) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                                    values = (idcve, published, priority, package, distro, support, status, resolved_date)
+                                    cursor.execute(sql, values)
+                                    mydb.commit()
+                                else:
+                                    # Write to DataBase
+                                    sql = "INSERT INTO ubuntu (CVE, Published, Priority, Package, Distro, Support, Status) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                                    values = (idcve, published, priority, package, distro, support, status)
+                                    cursor.execute(sql, values)
+                                    mydb.commit()
+                                    
         except requests.exceptions.RequestException as e:
             print(f"Erro ao obter dados para {url}: {e}")
 print("Finished")
