738405395c9988375306f7d99daa04577c8a121e
Daniel <31888602+daniel-franca@users.noreply.github.com>
2025-02-19 16:14:32 -0300

    New Version

--- ubuntu_pro.sh
+++ ubuntu_pro.sh
@@ -7,7 +7,7 @@ IFS=","
 OUTPUT_CSV="results.csv"
 #
 # CSV Headers
-echo "CVE,Package,Distro,Support,MinDate,Resolved" > $OUTPUT_CSV \
+echo "CVE,Distro,Package,Support,Resolved" > $OUTPUT_CSV \
 ## Get Ubuntu´s codename
 ubuntu_codename=$(lsb_release -cs)
 echo $ubuntu_codename
@@ -17,12 +17,12 @@ while read f1 f2 f3 f4 f5
 do
    if [[ $ubuntu_codename == $f2 ]]; then
       #Package
-      echo $f1
-      #Verson
       echo $f3
-      apt install -y $f1
-      resolved=$(zgrep -A 1000 -e "$f3" /usr/share/doc/$f1/changelog.Debian.gz |grep -e '-- ' | head -n 1 | cut -d ">" -f 2-)
+      #Verson
+      echo $f4
+      apt install -y $f3
+      resolved=$(zgrep -A 1000 -e "$f4" /usr/share/doc/$f3/changelog.Debian.gz |grep -e '-- ' | head -n 1 | cut -d ">" -f 2-)
       # Saving file
-      echo $f5,$f1,$f2,$f3,$f4,$resolved >> $OUTPUT_CSV
+      echo $f1,$f2,$f3,$f4,$resolved >> $OUTPUT_CSV
    fi
 done < ubuntu_pro.csv
